cfb9f1a55de7fa0d721a1ee1f3a8271d8b34394e
Elias <32038438+elisor1219@users.noreply.github.com>
2022-11-14 14:24:41 +0100

    Changed the break bindning part.

--- Attacker.java
+++ Attacker.java
@@ -23,18 +23,24 @@ public class Attacker {
         return this.X;
     }
 
-    protected String[] breakBinding(String shortCommit) throws NoSuchAlgorithmException{
+    protected String[][] breakBinding(String shortCommit) throws NoSuchAlgorithmException{
         setX(shortCommit.length());
         setCS(getX());
 
         int randValUpTo = maxSearchBreakBinding; 
 
-        //So the first collumn is the message 0 and the second message 1
-        String[] randVal = {"", ""};
+        //The first array/collumn is the key and the second when found, for 0
+        //The third array/collumn is the key and the fourth when found, for 0
+        String[][] randVal = {{,}, {,}, {,}, {,}};
+        String[][] tempVar = {{},{}};
         //Start by guessing message = 0;
-        randVal[0] = findRand(shortCommit, "0", randValUpTo);
+        tempVar = findRand(shortCommit, "0", randValUpTo);
+        randVal[0] = tempVar[0];
+        randVal[1] = tempVar[1];
         //Then by guessing message = 1;
-        randVal[1] = findRand(shortCommit, "1", randValUpTo);
+        tempVar = findRand(shortCommit, "1", randValUpTo);
+        randVal[2] = tempVar[0];
+        randVal[3] = tempVar[1];
         return randVal;
     }
 
@@ -76,35 +82,57 @@ public class Attacker {
         return foundMessage;
     }
 
-    protected String findRand (String shortCommit, String guessMessage, int randValUpTo) throws NoSuchAlgorithmException{
+    protected String[][] findRand (String shortCommit, String guessMessage, int randValUpTo) throws NoSuchAlgorithmException{
         String guessRandVal;
         String guessCommit = "-1";
+        //{{},{}};
+        //The right way of doing this would be using a arrayList
+        //Dont have time to fix this, but would hep to have an list!
+        String[][] rightCommits = new String[2][randValUpTo];
 
-        int i = 0;
-        while (true) {
-            if (i > randValUpTo){
-                if (loadBarOn){
-                    System.out.println("Search aborted at i = " + i);
-                }
-                return "-1";
-            }
+        int j = 0;
+        for (int i = 0; i < randValUpTo; i++) {
             guessRandVal = Integer.toBinaryString(i);
 
             guessCommit = CS.commit(guessMessage, guessRandVal);
             if(shortCommit.equals(guessCommit)){
                 if (debugMode){
-                System.out.println("Found randVal in " + i + " steps.");
-                System.out.println("RandVal: " + guessRandVal);
+                System.out.println("Found one key in " + i + " steps.");
+                System.out.println("Key: " + guessRandVal);
                 }
                 if (loadBarOn){
-                    System.out.println("Found randVal at i = " + i);
+                    System.out.println("Found one key at i = " + i);
                 }
-                return guessRandVal;
+                //Stores the guess key in the first array and
+                //when it was found in the second array.
+                rightCommits[0][j+1] = guessRandVal;
+                rightCommits[1][j+1] = Integer.toString(i);
+                j++;
             }
             i++;
             if (loadBarOn && Math.floorMod(i, 500000) == 0){
                 System.out.println("Search index = " +  i);
             }
         }
+
+        //Here it is not neccecary to have a if, but I only need the code to
+        //work once... For now
+        if (j == 0){
+            //String[][] retVal = {{"Total","-1"}, {Integer.toString(j), Integer.toString(j)}};
+            //return retVal;
+            rightCommits[0][0] = "Total";
+            rightCommits[1][0] = "0";
+            return rightCommits;
+        } else {
+            //for (String val : rightCommits[0]) {
+            //    if (val.equals("null"))
+            //}
+            rightCommits[0][0] = "Total";
+            rightCommits[1][0] = Integer.toString(j);
+            return rightCommits;
+        }
+
+
+
     }
 }
--- MainFile.java
+++ MainFile.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class MainFile {
     static int K = 16;
     //To not use the trunc. function, set this to a large number, i.e. 100000
-    static int X = 15;
+    static int X = 1;
     static boolean debugMode = false;
     public static void main(String[] args) throws NoSuchAlgorithmException {
         System.out.println("\n----------------------------------------------");
@@ -23,10 +23,10 @@ public class MainFile {
         }
 
         int rightGuesses = 0;
-        int wrongguesses = message[0].length*2;
+        int wrongGuesses = message[0].length*2;
         for (int i = 0; i < message[0].length; i++) {
             rightGuesses += message[Integer.parseInt(originalString)][i];
-            wrongguesses -= message[Integer.parseInt(originalString)][i];
+            wrongGuesses -= message[Integer.parseInt(originalString)][i];
         }
         
 
@@ -35,29 +35,39 @@ public class MainFile {
         System.out.println("Commit:             " + shortCommit);
 
         System.out.println("Right guesses:  " + rightGuesses);
-        System.out.println("Wronng guesses: " + wrongguesses);
+        System.out.println("Wronng guesses: " + wrongGuesses);
 
         System.out.println("Ratio: " + (double) rightGuesses/(message[0].length*2));
 
-        System.out.println("1/(2^" + X +") = " + 1/(Math.pow(2,X)* 2));
+        System.out.println("1/(2^" + X +" * 2) = 2^-(" + X + "+1) = " + 1/(Math.pow(2,X)* 2));
 
         //---Second part---
         System.out.println("\n-----Second part-----");
-        String[] shortCommitChanged = {"", ""};
+        //String[] shortCommitChanged = {"", ""};
 
-        String[] randValStringGuess = adversary.breakBinding(shortCommit);
+        String[][] randValStringGuess = adversary.breakBinding(shortCommit);
+        int rightKeys = Integer.parseInt(randValStringGuess[1][0]) 
+                        + Integer.parseInt(randValStringGuess[3][0]);
+        int totalTries = (randValStringGuess[0].length);
 
-        for (int i = 0; i < randValStringGuess.length; i++) {
-            if (randValStringGuess[i].equals("-1")){
-                shortCommitChanged[i] = "Aborted. Exceeded time limit";
-            }else{
-                shortCommitChanged[i] = CS.commit(Integer.toString(i), randValStringGuess[i]);
-            }
-        }
+        System.out.println("Right keys found: " + rightKeys);
+        System.out.println("Total tries:      " + totalTries);
 
-        System.out.println("Original commit with " + originalString + " : " + shortCommit);
-        System.out.println("Commit changed to 0: " + shortCommitChanged[0]);
-        System.out.println("Commit changed to 1: " + shortCommitChanged[1]);
+        System.out.println("Ratio:            " + (double) rightKeys/totalTries);
+
+        System.out.println("1/(2^" + X +") = 2^-" + X +" = " + 1/(Math.pow(2,X)));
+
+        //for (int i = 0; i < randValStringGuess.length; i++) {
+        //    if (randValStringGuess[i][0].equals("-1")){
+        //        shortCommitChanged[i] = "Aborted. Exceeded time limit";
+        //    }else{
+        //        shortCommitChanged[i] = CS.commit(Integer.toString(i), randValStringGuess[i][0]);
+        //    }
+        //}
+
+        //System.out.println("Original commit with " + originalString + " : " + shortCommit);
+        //System.out.println("Commit changed to 0: " + shortCommitChanged[0] + " \t Itterations = " + randValStringGuess[0][1]);
+        //System.out.println("Commit changed to 1: " + shortCommitChanged[1] + " \t Itterations = " + randValStringGuess[1][1]);
     }
 
     private static String randBinary(int numberOfNum){
