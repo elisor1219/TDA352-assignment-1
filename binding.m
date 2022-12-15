d4e353191189df5ff6bf360224b444fa3688e197
Elias Sörstrand <32038438+elisor1219@users.noreply.github.com>
2022-12-16 00:56:41 +0100

    Faster version done

--- binding.m
+++ binding.m
@@ -82,8 +82,8 @@ clc;clf;clear;
 tic
 redBalls = 2^16;         %16 is from the assignment
 greenBalls = 2^16;
-goForIterations = 1:2;
-XIterations = 7:22;     %From assignment X \in [0,30]
+goForIterations = 1:1000;
+XIterations = 0:20;     %From assignment X \in [0,30]
 
 bindingBroken = zeros(size(goForIterations,2), size(XIterations,2));
 
@@ -91,7 +91,7 @@ for i = XIterations
     X = i;
     bins = 2.^X;
     i
-
+    tic
     for j = goForIterations
         %Simulat throwing 2^16 red balls (row 1) and throwing 2^16 green 
         % balls (row 2).
@@ -115,10 +115,49 @@ for i = XIterations
         binHasBalls = numberOfBalsInEachBin >= 1;
         
         %Determiin if a bin has at least on red and green ball in it
-        binHasOverflow = sum(binHasBalls,1) >= 2;
+        binHasOverflow = sum(binHasBalls, 1) >= 2;
         
         %If any bin has more then one sort of ball, the binding is borken
-        if sum(binHasOverflow) >= 1
+        if sum(binHasOverflow, 2) >= 1
+            bindingBroken(j,i+1) = 1;
+        end
+    end
+    toc
+end
+
+
+bindingBrokenProb = sum(bindingBroken,1)./size(bindingBroken,1);
+
+plot(XIterations, bindingBrokenProb(XIterations+1));
+toc
+
+%% Version 3
+clc;clf;clear;
+
+%Throw total of 2^17 balls
+%2^16 red balls
+tic
+redBalls = 2^16;         %16 is from the assignment
+greenBalls = 2^16;
+goForIterations = 1:1000;
+XIterations = 0:40;     %From assignment X \in [0,30]
+
+bindingBroken = zeros(size(goForIterations,2), size(XIterations,2));
+
+parfor i = XIterations
+    X = i;
+    bins = 2.^X;
+    i
+
+    for j = goForIterations
+        %Simulat throwing 2^16 red balls (row 1) and throwing 2^16 green 
+        % balls (row 2).
+        %Starting at 1 because 2^0 = 1
+        %Row is color of ball, column is which ball, value is in which bin
+        ballsInWhichBin = randUniform(1, bins, 2, redBalls);
+
+        collisions = ismember(ballsInWhichBin(1,:), ballsInWhichBin(2,:));
+        if sum(collisions, 2) >= 1
             bindingBroken(j,i+1) = 1;
         end
     end
@@ -129,4 +168,127 @@ bindingBrokenProb = sum(bindingBroken,1)./size(bindingBroken,1);
 
 plot(XIterations, bindingBrokenProb(XIterations+1));
 toc
+
+%% Version 4
+clc;clf;clear;
+
+%Throw total of 2^17 balls
+%2^16 red balls
+tic
+redBalls = 2^16;         %16 is from the assignment
+greenBalls = 2^16;
+goForIterations = 1:3000;
+XIterations = 40:40;     %From assignment X \in [0,30]
+
+bindingBroken = zeros(size(goForIterations,2), size(XIterations,2));
+
+for i = XIterations
+    X = i;
+    bins = 2.^X;
+    i
+
+    tic
+    for j = goForIterations
+        ballsInWhichBin = randUniform(1, bins, 2, redBalls);
+        ballsInWhichBin = sort(ballsInWhichBin,2);
+        
+        pRed = 1;
+        pGreen = 1;
+        for k = 1:size(ballsInWhichBin,2)
+            if ballsInWhichBin(1,pRed) < ballsInWhichBin(2,pGreen)
+                pRed = pRed + 1;
+                continue
+            elseif ballsInWhichBin(1,pRed) > ballsInWhichBin(2,pGreen)
+                pGreen = pGreen + 1;
+                continue
+            else
+                bindingBroken(j,i+1) = 1;
+                break;
+            end
+        end
+    end
+    toc
+end
+
+
+bindingBrokenProb = sum(bindingBroken,1)./size(bindingBroken,1);
+toc
+%% Plot for v4
+clf;
+plot(XIterations, bindingBrokenProb(XIterations+1), "LineWidth", 2);
+grid on
+
+legend("Overfull bins", "FontSize",17, 'Location','southwest')
+title("Probability of breaking the binding property", "FontSize",17)
+xlabel("X = truncation point", "FontSize",17)
+ylabel("Probability", "FontSize",17)
+axis([0 XIterations(end) 0 1.1])
+
+saveas(gcf, "bindingBinsV4.png");
+
+%% Version 5 worse then before
+clc;clf;clear;
+
+%Throw total of 2^17 balls
+%2^16 red balls
+tic
+redBalls = 2^16;         %16 is from the assignment
+greenBalls = 2^16;
+goForIterations = 1:100;
+XIterations = 0:40;     %From assignment X \in [0,30]
+
+bindingBroken = zeros(size(goForIterations,2), size(XIterations,2));
+
+parfor i = XIterations
+    X = i;
+    bins = 2.^X;
+    i
+
+    ballsInWhichBin = randUniform(1, bins, 2*size(goForIterations,2), redBalls);
+    ballsInWhichBin = sort(ballsInWhichBin,2);
+    
+    for j = goForIterations
+        pRed = 1;
+        pGreen = 1;
+        for k = 1:size(ballsInWhichBin,2)
+            if ballsInWhichBin(j,pRed) < ballsInWhichBin(size(goForIterations,2) + j,pGreen)
+                pRed = pRed + 1;
+                continue
+            elseif ballsInWhichBin(j,pRed) > ballsInWhichBin(size(goForIterations,2) + j,pGreen)
+                pGreen = pGreen + 1;
+                continue
+            else
+                bindingBroken(j,i+1) = 1;
+                break;
+            end
+        end
+    end
+end
+
+
+bindingBrokenProb = sum(bindingBroken,1)./size(bindingBroken,1);
+
+plot(XIterations, bindingBrokenProb(XIterations+1));
+toc
+
+%%
+A = [1 2 5 1, 3 6 7 8];
+[uniqueA i j] = unique(A,'first');
+indexToDupes = find(not(ismember(1:numel(A),i)))
+
+%%
+tic
+ballsInWhichBin = randUniform(1, bins, 2*1000, redBalls);
+toc
+
+
+
+
+
+
+
+
+
+
+
 
